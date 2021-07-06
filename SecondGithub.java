87b9c67a2050b91ae395229fdbceb86f0b08b24f
JuanK0 <jkh2.omg@gmail.com>
2021-07-05 23:04:58 -0500

    Hola

---
+++ SecondGithub.java
@@ -0,0 +1,15 @@
+public class SecondGithub{
+//Esto es una prueba del pull
+  
+//Esta es el método principal
+public static void main(String [] args){
+String name="Juan";
+int age=21;
+
+System.out.println("Greetings");
+System.out.println("Mi nombre es "+name+" .Tengo "+age+" años");
+
+}
+
+
+}
